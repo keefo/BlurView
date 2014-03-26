4307750952f40027a55afe153759c5b9d6ac93f9
Xu Lian <lianxu@beyondcow.com>
2014-03-25 22:51:37 -0700

    comment on blurimage1:

--- BlurView/LXBlurView.m
+++ BlurView/LXBlurView.m
@@ -130,7 +130,7 @@
 }
 
 
-//this blue method is for testing only
+//this blur method is for testing only
 - (NSImage*)blurimage1:(NSImage*)img withRadius:(CGFloat)radius withIteration:(int)iterations tintColor:(NSColor *)tintColor
 {
     //image must be nonzero size
